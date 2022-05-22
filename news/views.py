b93b8c01caaf02129d1089fecd95446523ebb5a5
juodasvarnas <101868391+juodasvarnas@users.noreply.github.com>
2022-05-22 19:32:00 +0300

    Initial commit

---
+++ news/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News, Category
+from .forms import NewsForms
+from django.views.generic import ListView
+
+
+# Create your views here.
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    # extra_context = {'title': 'Новостной сайт'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Новостной сайт'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForms(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            # news = News.objects.create(**form.cleaned_data)
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForms()
+    return render(request, 'news/add_news.html', {'form': form})
