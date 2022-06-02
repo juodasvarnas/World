f3fb6c83723341aaf89f69039fc5031532af6503
juodasvarnas <101868391+juodasvarnas@users.noreply.github.com>
2022-06-02 18:27:32 +0300

    asd

--- news/views.py
+++ news/views.py
@@ -19,7 +19,7 @@ class HomeNews(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related('category')
 
 
 class NewsByCategory(ListView):
